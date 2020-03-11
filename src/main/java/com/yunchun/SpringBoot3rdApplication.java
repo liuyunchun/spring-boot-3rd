dd16283ed95dd3f8275f1535d3a6c70b3e383898
yunchun <yunchun@tist.com.tw>
2020-03-11 15:09:51 +0800

    first commit

---
+++ src/main/java/com/yunchun/SpringBoot3rdApplication.java
@@ -0,0 +1,13 @@
+package com.yunchun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3rdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3rdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yunchun/SpringBoot3rdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunchun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3rdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
